235814461932fe1de567fe9638a5979ead8689f5
Bikram7852 <bikram.roy7852@gmail.com>
2023-10-26 18:38:08 +0530

    Add project files.

---
+++ SinglyLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedListDemo list = new SinglyLinkedListDemo();
+            list.CreateSinglyLinkedList();
+            Console.ReadLine();
+        }
+    }
+}
